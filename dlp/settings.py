38a65b190c9b29d3fbe4156833bc0648e511f140
akshin-autods <agshin@autods.com>
2024-11-06 23:08:06 +0100

    update: update DLP model settings

--- dlp/settings.py
+++ dlp/settings.py
@@ -6,5 +6,5 @@ REDIS_DB = int(os.environ.get("REDIS_DB", 0))
 
 QUEUE_NAME = os.environ.get("QUEUE_NAME")
 
-PATTERNS_URL = os.environ.get("PATTERNS_URL", "http://localhost:8000/patterns/")
-UPDATE_LEAKED_MESSAGE_URL = os.environ.get("UPDATE_URL", "http://localhost:8000/update_leaked_message/")
+PATTERNS_URL = os.environ.get("PATTERNS_URL", "http://localhost:8000/api/patterns/")
+UPDATE_MESSAGE_URL = os.environ.get("UPDATE_MESSAGE_URL", "http://localhost:8000/api/update_message/")
