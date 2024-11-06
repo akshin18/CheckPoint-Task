6021b96d419ed139b7cb918ef7462c263220b273
akshin-autods <agshin@autods.com>
2024-11-06 23:08:41 +0100

    update: update django urls

--- project/api/urls.py
+++ project/api/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path("", views.event_handler, name="event_handler"),
     path("get_patterns/", views.get_patterns, name="get_patterns"),
-    path("update_leaked_message/", views.update_leaked_message, name="update_leaked_message"),
+    path(
+        "update_message/",
+        views.update_message,
+        name="update_message",
+    ),
+    path("ping/", views.ping, name="ping"),
 ]
