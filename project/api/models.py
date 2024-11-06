5b2047731d24591f877f4a14f43de82340aa7eff
akshin-autods <agshin@autods.com>
2024-11-06 20:16:57 +0100

    update: update models.py

--- project/api/models.py
+++ project/api/models.py
@@ -25,6 +25,7 @@ class FlaggedMessage(BaseModel):
     client_msg_id = models.CharField(max_length=255, unique=True, default=None)
     content = models.TextField()
     matched_pattern = models.ForeignKey(Pattern, on_delete=models.CASCADE)
+    is_checked = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.matched_pattern.name} match at {self.created_at}"
